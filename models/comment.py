ccd49c064c62b2a2bd665e92315f6d51e91aa8b5
jiayan <jiayan@findourview.com>
2022-02-22 00:43:44 -0600

    finished post module

--- models/comment.py
+++ models/comment.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from . import db
+from .utils import get_display_time
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -29,5 +30,6 @@ class Comment(db.Model):
             'id': self.id,
             'text': self.text,
             'post_id': self.post_id,
-            'user': self.user.to_dict()
+            'user': self.user.to_dict(),
+            'display_time': get_display_time(self.pub_date)
         }
